4da0ea42d3809f253fa075c217a13b02c6a5c177
mmagni <matias.magni@gmail.com>
2021-05-11 16:32:36 -0300

    Fibonacci UI done.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const baseUrl = 'http://localhost:3000';
+
+const App = () => {
+
+  const [num, setNum] = useState(0);
+  const [fibonacciResult, setFibonacciResult] = useState(undefined);
+
+  const handleChange = (event) => {
+    setNum(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios.get(baseUrl + '/fibonacci?number=' + num)
+      .then((response) => {
+        // handle success
+        setFibonacciResult(response.data.result);
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      })
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +35,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Ingrese un número para calcular la secuencia de Fibonacci:
+            <input data-testid="num" id="num" type="num" name="num" value={num} onChange={handleChange} />
+          </label>
+          <input data-testid="calc" id="submitFibonacci" type="submit" value="Calcular" />
+          {fibonacciResult && <p data-testid="result">Resultado: {fibonacciResult}</p>}
+        </form>
+
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  it('App renders correctly!', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('App shows result after submitting a number.', async () => {
+    const { getByTestId, findByTestId } = render(<App />);
+    fireEvent.change(getByTestId('num'), { target: { value: 10 } });
+    fireEvent.click(getByTestId('calc'));
+    expect(await findByTestId('result')).toBeInTheDocument();
+    expect(await findByTestId('result')).toHaveTextContent('Resultado: 55');
+  });
 });
